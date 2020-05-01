200e2a60872f8feae6b6ce2c0e0eeaff98690254
Ishaan Sathaye <IshaanSathaye21@mittymonarch.com>
2020-04-30 18:21:01 -0700

    finished stock graphs

--- matplotlib/subplot & stocks.py
+++ matplotlib/subplot & stocks.py
@@ -7,9 +7,9 @@ import urllib.request
 from matplotlib import style
 
 style.use('fivethirtyeight')
-print(plt.style.available)
+# print(plt.style.available)
 
-print(plt.__file__)
+# print(plt.__file__)
 
 MA1 = 10
 MA2 = 30
@@ -32,10 +32,13 @@ def graph_data(days):
     fig = plt.figure()
     ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan=1, colspan=1)
     plt.title('Stock Info')
-    ax2 = plt.subplot2grid((6, 1), (1, 0), rowspan=4, colspan=1)
-    plt.xlabel('Date')
+    plt.ylabel('H-L')
+    ax2 = plt.subplot2grid((6, 1), (1, 0), rowspan=4, colspan=1, sharex=ax1)
+    # plt.xlabel('Date')
     plt.ylabel('Price')
-    ax3 = plt.subplot2grid((6, 1), (5, 0), rowspan=1, colspan=1)
+    ax2v = ax2.twinx() # truly share that x-axis
+    ax3 = plt.subplot2grid((6, 1), (5, 0), rowspan=1, colspan=1, sharex=ax1)
+    plt.ylabel('MovingAvgs')
 
     stockPrice = 'https://pythonprogramming.net/yahoo_finance_replacement'
     sourceCode = urllib.request.urlopen(stockPrice).read().decode()
@@ -67,34 +70,45 @@ def graph_data(days):
 
     h_l = list(map(highMinusLow, highp, lowp))
 
-    ax1.plot_date(date, h_l, '-')
+    ax1.plot_date(date[-start:], h_l[-start:], '-', label='H-L')
+    ax1.yaxis.set_major_locator(mticker.MaxNLocator(nbins=4, prune='lower'))
 
-    candlestick_ohlc(ax2, ohlc, width=0.4, colorup='g', colordown='r')
+    candlestick_ohlc(ax2, ohlc[-start:], width=0.4, colorup='g', colordown='r')
 
-    for label in ax2.xaxis.get_ticklabels():
-        label.set_rotation(45)
-    ax2.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d-%Y'))
-    ax2.xaxis.set_major_locator(mticker.MaxNLocator(10))
+    ax2.yaxis.set_major_locator(mticker.MaxNLocator(nbins=7, prune='upper'))
     ax2.grid(True)
 
     bbox_props = dict(boxstyle='round', fc='w', ec='k', lw=1)  # adding annotation for last price
-    ax2.annotate(str(closep[0]), (date[0], closep[0]), xytext=(date[0] + 1, closep[0]), bbox=bbox_props, fontsize='x-small')
+    ax2.annotate(str(closep[0]), (date[0], closep[0]), xytext=(date[-start] + 4, closep[-start]), bbox=bbox_props, fontsize='xx-small')
+    ax2v.plot([], [], color='#007983', alpha=0.4, label='Volume')
+    ax2v.fill_between(date[-start:], 0, volume[-start:], facecolor='#007983', alpha=0.4)
+    ax2v.axes.yaxis.set_ticklabels([])
+    ax2v.grid(False)
+    ax2v.set_ylim(0, 3*volume.max())
 
-    # annotation with arrows and text in graph
-    # fontDict = {'family': 'serif', 'color': 'darkred', 'size': 15}  # font dictionary
-    # ax2.text(date[20], closep[1], 'text example', fontdict=fontDict)  # putting text on the plot
-    #
-    # ax2.annotate('Good News', (date[11], highp[11]), xytext=(0.8, 0.9), textcoords='axes fraction',
-    #              arrowprops=dict(facecolor='grey', color='grey'))
-    # # annotation on the graph, fixes the annotation at the relative position, arrow moves
+    ax3.plot(date[-start:], ma1[-start:], linewidth=3, label=str(MA1)+'MA1')
+    ax3.plot(date[-start:], ma2[-start:], linewidth=3, label=str(MA2)+'MA2')
+    ax3.fill_between(date[-start:], ma2[-start:], ma1[-start:], where=(ma1[-start:] < ma2[-start:]), facecolor='r', edgecolor='r', alpha=0.5)
+    ax3.fill_between(date[-start:], ma2[-start:], ma1[-start:], where=(ma1[-start:] > ma2[-start:]), facecolor='g', edgecolor='g', alpha=0.5)
+    ax3.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d-%Y'))
+    ax3.xaxis.set_major_locator(mticker.MaxNLocator(10))
+    ax3.yaxis.set_major_locator(mticker.MaxNLocator(nbins=4, prune='upper'))
+    for label in ax3.xaxis.get_ticklabels():
+        label.set_rotation(45)
+    
 
-
-    ax3.plot(date[-start:], ma1[-start:])
-    ax3.plot(date[-start:], ma2[-start:])
-
-
-    # plt.legend()
-    plt.subplots_adjust(left=0.11, bottom=0.24, right=0.85, top=0.90, wspace=0.2, hspace=0)
+    plt.setp(ax1.get_xticklabels(), visible=False)
+    plt.setp(ax2.get_xticklabels(), visible=False)
+    plt.subplots_adjust(left=0.11, bottom=0.24, right=0.9, top=0.90, wspace=0.2, hspace=0)
+    ax1.legend()
+    leg = ax1.legend(loc=9, ncol=2, prop={'size':11})
+    leg.get_frame().set_alpha(0.4)
+    ax2v.legend()
+    leg = ax2v.legend(loc=9, ncol=2, prop={'size':11})
+    leg.get_frame().set_alpha(0.4)
+    ax3.legend()
+    leg = ax3.legend(loc=9, ncol=2, prop={'size':11})
+    leg.get_frame().set_alpha(0.4)
     plt.show()
 
 graph_data(days=input('How many days of stock history?: '))
